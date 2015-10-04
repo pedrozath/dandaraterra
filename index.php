1261741d68786e39a3639bd831a9296492768721
Pedro Maciel <pedrozath@gmail.com>
2015-10-04 11:31:01 -0300

    update

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
-<doctype html!>
+<!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="refresh" content="0; url=https://www.youtube.com/channel/UCTdfSUyLtDDG3fNsU3_MuFA" />
+    <meta http-equiv="refresh" content="0; url=https://www.youtube.com/channel/UCTdfSUyLtDDG3fNsU3_MuFA" />
+    <title></title>
 </head>
 <body>
+
 </body>
 </html>
