bc2424cac868a757b1d045db90beb8904536abe8
Pedro Maciel <pedrozath@gmail.com>
2015-12-17 03:47:25 -0200

    updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
+
 class PagesController < ApplicationController
     def index
+        @data = YAML.load_file(Rails.root.join "db", "feed.yml")
         render layout: false
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def markdown(string)
+        raw markdown_renderer.render string
+    end
+
+    def markdown_renderer
+        @markdown_renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(render_options = {}), extensions = {})
+        @markdown_renderer
+    end
 end
